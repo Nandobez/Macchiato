3a0daf38833990ad26814a816876129243919451
Nandobez <fernando.devsolutions13@gmail.com>
2025-12-22 20:18:00 -0300

    dsl: Element interface + Tag with fluent attrs

---
+++ src/main/java/dev/nandobez/macc/dsl/Element.java
@@ -0,0 +1,4 @@
+package dev.nandobez.macc.dsl;
+
+/** Anything that can be rendered as JSX. Sealed: Tag | Fragment | ExprNode | EachNode | PickNode | ComponentNode | TextNode | LazyNode. */
+public interface Element {}
