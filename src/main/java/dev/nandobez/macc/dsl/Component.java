5d4d79ce70b057193f0c8a79f617a67af958bb02
Nandobez <fernando.devsolutions13@gmail.com>
2026-01-13 20:55:00 -0300

    dsl: ~40 HTML tag factories + Helpers ($/expr, each, pick, group)

---
+++ src/main/java/dev/nandobez/macc/dsl/Component.java
@@ -0,0 +1,6 @@
+package dev.nandobez.macc.dsl;
+
+/** Base class for user components. Subclasses override Element render(). */
+public abstract class Component {
+    public abstract Element render();
+}
